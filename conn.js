4f03ff6ab862d409571891e5ae5456b937c89258
Md Bakaulllah <77727469+Bakaullah@users.noreply.github.com>
2021-02-07 19:23:44 +0530

    Rest API using Node.js and mongoose
    I have develop a restful API of 100m race athlete winner ranking using Node.js, expresss.js, Postman App and mongoDB.

---
+++ conn.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/olympics", { 
+    useCreateIndex: true,
+    useNewUrlParser: true, 
+    useUnifiedTopology:true
+
+}).then(() => {
+    console.log("connection is successful")
+    
+}).catch((err) => {
+    console.log( err, "No connection");
+});
