e6b722f8f5c1123e3e56c732b9a7b8dfa975ff46
Md Bakaulllah <77727469+Bakaullah@users.noreply.github.com>
2021-02-15 22:08:07 +0530

    Registration Form
    I have done a project of registration form using HTML, CSS, Node.js , Express and MongoDB. where users can register himself on the registration form and can store their data to MongoDB database.

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "./Style.css";
-import App from './App';
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 
 
-ReactDOM.render(
-  <>
+mongoose.connect('mongodb://localhost:27017/signupdb',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then( ()=>{console.log('db is connected')
+}).catch( (err)=>{
+    console.log(err)
+})
+const db = mongoose.connection;
 
-    <h2>DIGITAL WATCH USING REACT JS ( miniProject)</h2>
+app.use(bodyParser.urlencoded({ 
+    extended: true
+}));
 
-    <App />
-  </>,
-  document.getElementById('root')
-);
+app.use(bodyParser.json());
 
+app.use(express.static('public'));
+
+app.post("/signup", (req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phone = req.body.phone;
+    var password = req.body.password;
+
+    var data ={
+        "name" : name,
+        "email": email,
+        "phone":phone,
+        "password":password
+
+    }
+
+    db.collection('users').insertOne(data,(err, collection)=>{
+        if (err){
+            throw err;
+        }
+        console.log("Record is inserted successfully")
+    });
+    return res.redirect('singnup_success.html');
+
+})
+
+app.get('/', (req, res)=>{
+    res.render('index.js')
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port no ${port}`)
+})
